5976960d830cc5049127b7c96eb2a223b97dbb22
Unish Rajkarnikar <ubidy947@gmail.com>
2024-03-29 12:39:47 -0500

    first commit

---
+++ src/main/java/org/example/springazuredemo/SpringAzureDemoApplication.java
@@ -0,0 +1,21 @@
+package org.example.springazuredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringAzureDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAzureDemoApplication.class, args);
+    }
+
+    @GetMapping("/message")
+    public String message() {
+        return "Azure Demo Message";
+    }
+
+}
---
+++ src/test/java/org/example/springazuredemo/SpringAzureDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springazuredemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAzureDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
